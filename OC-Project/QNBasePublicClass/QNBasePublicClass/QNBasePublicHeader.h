f5def4b10edc49c43bf2481422e0e57b7cd0f3fe
qifeng <qifeng.sun@fetech.cn>
2018-09-27 02:22:06 +0800

    增加相关公用类文件

--- OC-Project/QNBasePublicClass/QNBasePublicClass/QNBasePublicHeader.h
+++ OC-Project/QNBasePublicClass/QNBasePublicClass/QNBasePublicHeader.h
@@ -9,5 +9,55 @@
 #ifndef QNBasePublicHeader_h
 #define QNBasePublicHeader_h
 
+/*
+ 宏定义 相关文件
+ */
+#import "MacroDefinitionHeader.h"
+
+/*
+ 页面相关
+ */
+#import "BaseNavigationBar.h"
+
+#import "BaseNavigationViewController.h"
+#import "BaseTabbarViewController.h"
+
+#import "BaseViewController.h"
+#import "MenuViewController.h"
+
+
+/*
+ 网络请求相关
+ */
+#import "AssistiveTouch.h"
+#import "UpdateUrlTableViewController.h"
+
+/*
+ CoreData 项目存储方法
+ */
+#import "CoreDataPublicManager.h"
+
+/*
+ 基础方法 类库
+ */
+#import "FEBaseTools.h"
+#import "CalculateSize.h"
+
+/*
+ 基础网络请求  及相关方法
+ */
+#import "Base_URLClass.h"
+
+/*
+ 基础线程相关方法 封装
+ */
+#import "QFBaseGroup.h"
+
+/*
+ 获取系统版本号及相关信息
+ */
+#import "VersionClass.h"
+
+
 
 #endif /* QNBasePublicHeader_h */
